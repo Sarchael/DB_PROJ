d45d0aae97785d856ec2077bedcc840b0e0e3482
szymon.szpakowski <szymon.szpakowski6@gmail.com>
2019-05-04 14:04:49 +0200

    Klient sketch and dostawca impl

--- src/main/java/com/project/bootfx/app/entity/Dostawca.java
+++ src/main/java/com/project/bootfx/app/entity/Dostawca.java
@@ -17,5 +17,67 @@ public class Dostawca {
     @Column(name ="nip")
     private String nip;
 
+    @Column(name ="nr_tel")
+    private String nr_tel;
 
+    @ManyToOne
+    @JoinColumn(name ="id_miasta")
+    private Miasto miasto;
+
+    Dostawca(){
+
+    }
+
+    Dostawca(String nazwa, String nip, String nr_tel){
+        this.nazwa = nazwa;
+        this.nip = nip;
+        this.nr_tel = nr_tel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public String getNr_tel() {
+        return nr_tel;
+    }
+
+    public void setNr_tel(String nr_tel) {
+        this.nr_tel = nr_tel;
+    }
+
+    public Miasto getMiasto() {
+        return miasto;
+    }
+
+    public void setMiasto(Miasto miasto) {
+        this.miasto = miasto;
+    }
+
+    @Override
+    public String toString(){
+        return "Dostawca{" +
+                "id= " + id
+                +", nazwa= " + nazwa;
+    }
 }
