ef3b1ff2cc12bdfc9ff5d9f29620a5b79fcdfbfb
Michał Pocheć (michpoc547) <michpoc547@student.polsl.pl>
2019-05-04 14:03:49 +0200

    Dodano Model, Naprawa, Samochod (prawie skonczone)

--- src/main/java/com/project/bootfx/app/entity/Naprawa.java
+++ src/main/java/com/project/bootfx/app/entity/Naprawa.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
-@Table(name="naprawa")
+@Table(name = "naprawa")
 public class Naprawa {
 
     @Id
@@ -15,7 +15,7 @@ public class Naprawa {
     @Column(name = "opis")
     private String opis;
 
-    @Column(name="czas_naprawy")
+    @Column(name = "czas_naprawy")
     private int czasNaprawy;
 
     @Column(name = "data")
@@ -24,7 +24,90 @@ public class Naprawa {
     @Column(name = "postep")
     private String postep;
 
+    @ManyToOne
+    @JoinColumn(name = "id_sam")
     private Samochod samochod;
 
+    @ManyToOne
+    @JoinColumn(name = "id_prac_odp")
     private Pracownik pracownik;
+
+    public Naprawa() {
+    }
+
+    public Naprawa(String opis, int czasNaprawy, Date data, String postep) {
+        this.opis = opis;
+        this.czasNaprawy = czasNaprawy;
+        this.data = data;
+        this.postep = postep;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public int getCzasNaprawy() {
+        return czasNaprawy;
+    }
+
+    public void setCzasNaprawy(int czasNaprawy) {
+        this.czasNaprawy = czasNaprawy;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getPostep() {
+        return postep;
+    }
+
+    public void setPostep(String postep) {
+        this.postep = postep;
+    }
+
+    public Samochod getSamochod() {
+        return samochod;
+    }
+
+    public void setSamochod(Samochod samochod) {
+        this.samochod = samochod;
+    }
+
+    public Pracownik getPracownik() {
+        return pracownik;
+    }
+
+    public void setPracownik(Pracownik pracownik) {
+        this.pracownik = pracownik;
+    }
+
+    @Override
+    public String toString() {
+        return "Naprawa{" +
+                "id=" + id +
+                ", opis='" + opis + '\'' +
+                ", czasNaprawy=" + czasNaprawy +
+                ", data=" + data +
+                ", postep='" + postep + '\'' +
+                ", samochod=" + samochod +
+                ", pracownik=" + pracownik +
+                '}';
+    }
 }
--- src/main/java/com/project/bootfx/app/entity/Samochod.java
+++ src/main/java/com/project/bootfx/app/entity/Samochod.java
@@ -1,4 +1,112 @@
 package com.project.bootfx.app.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="samochod")
 public class Samochod {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="nr_rej")
+    private String numerRejestracyjny;
+
+    @Column(name="vin")
+    private String vin;
+
+    @Column(name="rok_prod")
+    private int rokProdukcji;
+
+    @Column(name = "przebieg")
+    private int przebieg;
+
+    @ManyToOne
+    @JoinColumn(name = "id_mod")
+    private Model model;
+
+    @ManyToOne
+    @JoinColumn(name="id_kl")
+    private Klient klient;
+
+    public Samochod() {
+    }
+
+    public Samochod(String numerRejestracyjny, String vin, int rokProdukcji, int przebieg) {
+        this.numerRejestracyjny = numerRejestracyjny;
+        this.vin = vin;
+        this.rokProdukcji = rokProdukcji;
+        this.przebieg = przebieg;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumerRejestracyjny() {
+        return numerRejestracyjny;
+    }
+
+    public void setNumerRejestracyjny(String numerRejestracyjny) {
+        this.numerRejestracyjny = numerRejestracyjny;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public int getRokProdukcji() {
+        return rokProdukcji;
+    }
+
+    public void setRokProdukcji(int rokProdukcji) {
+        this.rokProdukcji = rokProdukcji;
+    }
+
+    public int getPrzebieg() {
+        return przebieg;
+    }
+
+    public void setPrzebieg(int przebieg) {
+        this.przebieg = przebieg;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public Klient getKlient() {
+        return klient;
+    }
+
+    public void setKlient(Klient klient) {
+        this.klient = klient;
+    }
+
+    @Override
+    public String toString() {
+        return "Samochod{" +
+                "id=" + id +
+                ", numerRejestracyjny='" + numerRejestracyjny + '\'' +
+                ", vin='" + vin + '\'' +
+                ", rokProdukcji=" + rokProdukcji +
+                ", przebieg=" + przebieg +
+                ", model=" + model +
+                ", klient=" + klient +
+                '}';
+    }
 }
